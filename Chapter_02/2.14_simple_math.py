f1889ea783c83c1bbd3bb1b592c1f244c1774044
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-04-30 23:18:47 +0300

    new

---
+++ Chapter_02/2.14_simple_math.py
@@ -0,0 +1,11 @@
+# Assign a value to a variable salary
+salary = 25000.0
+
+# Assign a value to a variable bonus
+bonus = 12000.0
+
+# Calculate wages before deductions by adding salary and bonus. Assign the result to a variable pay
+pay = salary + bonus
+
+# Print a variable pay
+print('Your salary is: ', pay)
---
+++ Chapter_02/2.15_sale_price.py
@@ -0,0 +1,4 @@
+original_price = float(input('Enter the original price of the product: '))
+discount = original_price * 0.2
+sale_price = original_price - discount
+print("The discounted price is: ", sale_price)
