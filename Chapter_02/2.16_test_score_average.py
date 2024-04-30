ece1d4ff2b01f5e141fb7f3845a6ad101cc48a1c
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-04-30 23:34:16 +0300

    add some new

---
+++ Chapter_02/2.16_test_score_average.py
@@ -0,0 +1,11 @@
+# Get three school grades and assign them to variables
+# test1 test2 test3
+test1 = float(input('Enter the first grade: '))
+test2 = float(input('Enter the second grade: '))
+test3 = float(input('Enter the third grade: '))
+
+# calculate the average score from three grades and assign result to variable average
+average = (test1 + test2 + test3) / 3.0
+
+# show variable average
+print('The average grade is', average)
