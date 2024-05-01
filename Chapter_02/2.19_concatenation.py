cd7d4b8d9e8f0aedc202feaf3067890352ceae66
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-05-01 16:21:42 +0300

    concatenation

---
+++ Chapter_02/2.19_concatenation.py
@@ -0,0 +1,7 @@
+# this program shows a concatenation
+first_name = input('Write your firstname: ')
+last_name = input('Write your lastname: ')
+
+full_name = first_name + ' ' + last_name
+
+print(full_name)
