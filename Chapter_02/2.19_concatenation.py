30f8937dbf05ff983a7f7e52438c9300a40d54b5
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-05-01 16:30:36 +0300

    concatenation update

--- Chapter_02/2.19_concatenation.py
+++ Chapter_02/2.19_concatenation.py
@@ -1,7 +1,14 @@
-# this program shows a concatenation
+this program shows a concatenation
 first_name = input('Write your firstname: ')
 last_name = input('Write your lastname: ')
 
 full_name = first_name + ' ' + last_name
 
 print(full_name)
+
+print('Введите объем '
+      'продаж за каждый день и '
+      'нажмите Enter.')
+
+print('1' + '2')
+print('п' + 'р' + 'и' + 'в' + 'е' + 'т')
