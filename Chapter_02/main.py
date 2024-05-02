3d8aba1a8d386474701a685a88e35054045a60da
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-05-02 14:54:30 +0300

    mmm

--- Chapter_02/main.py
+++ Chapter_02/main.py
@@ -11,5 +11,11 @@
 # number = 1234567890.123456
 # print(f'{number:,.2f}')
 
-number = 12345.6789
-print(f'The number is equal to {number:12,.2f} ')
+# number = 12345.6789
+# print(f'The number is equal to {number:12,.2f} ')
+
+# number = 22
+# print(f'The number is equal {number:10}')
+
+name = 'Ruslan'
+print(f'Hello {name:10}. Nice to meet you!')
