b2d1cc1b6a9d5b324f8707c23dd535c69d22a8c5
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-05-11 22:40:56 +0300

    mmm

---
+++ Chapter_02/2.24_center_align.py
@@ -0,0 +1,15 @@
+# This program demonstrates center aligning string literals
+name1 = 'Gordon'
+name2 = 'Smith'
+name3 = 'Washington'
+name4 = 'Alvarado'
+name5 = 'Livingston'
+name6 = 'Jones'
+
+# displaying Names On Screen
+print(f'***{name1:^20}***')
+print(f'***{name2:^20}***')
+print(f'***{name3:^20}***')
+print(f'***{name4:^20}***')
+print(f'***{name5:^20}***')
+print(f'***{name6:^20}***')
--- Chapter_02/main.py
+++ Chapter_02/main.py
@@ -17,5 +17,43 @@
 # number = 22
 # print(f'The number is equal {number:10}')
 
-name = 'Ruslan'
-print(f'Hello {name:10}. Nice to meet you!')
+# name = 'Ruslan'
+# print(f'Hello {name:10}. Nice to meet you!')
+
+# number = 99
+# print(f'{number:<10d}')
+#
+# total = 3.14
+# print(f'{total:>20.2f}')
+
+# # Порядок следования условных обозначений
+# print(f'number:^10,.2f') # correct
+# print(f'number:10^,.2f') # not correct
+
+# Concatenation of f-strings
+# name = 'Abby Lloyd'
+# department = 'Marketing'
+# position = 'Manager'
+# print(f'Employyee name: {name}, ' +
+#       f'Department: {department}, ' +
+#       f'Position: {position}.')
+
+# name = 'Carly'
+# print('Hello, {name}')  # not correct without f string
+# print(f'Hello, {name}') # correct with f string
+
+# value = 99
+# print(f'Value is equal {value + 1}')
+# value = 65.4321
+# print(f'Value is equal {value:.2f}')
+# value = 987654.129
+# print(f'Value is equal {value:,.2f}')
+# value = 9876543210
+# print(f'Value is equal {value:,d}')
+
+
+# name = 'Ruslan'
+# print(f'{name:10}')
+
+number = 0.2
+print(f'{number:%}')
